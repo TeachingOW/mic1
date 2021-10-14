cdac7bb4b5cc916bdc3597965d7bd9b2096f658d
MK <m.elbasiouny@gmail.com>
2021-10-14 00:19:11 -0400

    intial commit

---
+++ Readme.md
@@ -0,0 +1,26 @@
+This is orginally from the book
+
+
+You can run the program using the binary (included in directory bin) using java -jar mic1-0.0.1-SNAPSHOT.jar
+
+
+
+
+To build the code
+you need Apache maven
+
+mvn package
+
+
+To run 
+
+
+cd target
+
+java -jar mic1-0.0.1-SNAPSHOT.jar
+
+
+
+
+
+
---
+++ src/main/resources/Mic1.hs
@@ -0,0 +1,28 @@
+<?xml version='1.0' encoding='ISO-8859-1' ?>
+<!DOCTYPE helpset
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
+         "../dtd/helpset_2_0.dtd">
+
+<?MyFavoriteApplication this is data for my favorite application ?>
+
+<helpset version="1.0">
+
+  <!-- title -->
+  <title>Mic1MMV Help</title>
+
+  <!-- maps -->
+  <maps>
+     <homeID>title_page</homeID>
+     <mapref location="Map.jhm"/>
+  </maps>
+
+  <!-- views -->
+  <view>
+    <name>TOC</name>
+    <label>Mic1MMV Help</label>
+    <type>javax.help.TOCView</type>
+    <data>Mic1TOC.xml</data>
+  </view>
+
+
+</helpset>
